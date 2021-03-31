915920ae11da0ff0e980d1e76a1abe401a6bc69b
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2021-03-31 20:47:33 -0300

    Cria migration para inserção de usuário admin inicial de teste

---
+++ database/migrations/2021_03_31_232131_add_user_admin_to_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserAdminToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::table('users')->insert(
+            array(
+                'name' => 'Admin',
+                'email' => 'admin@mail.com',
+                'password' => bcrypt('123456'),
+                'admin' => 'S'
+            )
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->truncate();
+        });
+    }
+}
